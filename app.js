ebaa612e9652396f64ba7578ee87870d4fe942bc
maysam <maysam82.dev@gmail.com>
2020-08-14 00:56:28 +1000

    Added PORT to app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const cors = require('cors');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const cors = require("cors");
 const app = express();
+
+const PORT = process.env.port || 5000;
 
 // Allow cross-origin request
 app.use(cors());
 // create a middleware as a single route. and this route will be like an end point to interact with endpoint data . graphqlHTTP will be called as soon as getting a request to /graphql route
 app.use(
-	'/graphql',
-	graphqlHTTP({
-		// shcema should be passed here
-		schema,
-		graphiql: true,
-	})
+  "/graphql",
+  graphqlHTTP({
+    // shcema should be passed here
+    schema,
+    graphiql: true,
+  })
 );
 
-app.listen(4000, () => {
-	console.log('Listening for requests on port 4000');
+app.listen(PORT, () => {
+  console.log(`Listening for requests on port ${PORT}`);
 });
